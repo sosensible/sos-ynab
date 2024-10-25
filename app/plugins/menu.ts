0d13771b60d80d2e9d9ba028e6c5df94ff11ae7d
John Farrar <johnfarrar@sosapps.com>
2024-10-25 14:44:40 -0400

    early mods

--- app/plugins/menu.ts
+++ app/plugins/menu.ts
@@ -4,8 +4,8 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('app:mounted', () => {
     const menuStore = useMenuStore()
 
-    menuStore.addMenu({ id: 5, name: 'YNAB', link: '/ynab', children: [
-      { id: 1, name: 'About', link: '/ynab/about' }
+    menuStore.addMenu({ id: 5, name: 'YNAB', link: '/ynab', meta: { active: true }, children: [
+      { id: 1, name: 'About', link: '/ynab/about', meta: { active: true } }
     ]})
   })
 })
